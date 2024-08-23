a87a73a5a77671ae9e1a7ef194febcdaed2abe9d
xlei1123 <983171730@qq.com>
2024-08-23 19:42:58 +0800

    feat: 页面

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -17,15 +17,7 @@
     <div class="video">
       <h4>演示：</h4>
       <video controls width="1000">
-        <source src="/media/cc0-videos/flower.webm" type="video/webm" />
-
-        <source src="/media/cc0-videos/flower.mp4" type="video/mp4" />
-
-        Download the
-        <a href="/media/cc0-videos/flower.webm">WEBM</a>
-        or
-        <a href="/media/cc0-videos/flower.mp4">MP4</a>
-        video.
+        <source src="/video.mp4" type="video/mp4" />
       </video>
     </div>
   </div>
